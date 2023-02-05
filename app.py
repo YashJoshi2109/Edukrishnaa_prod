c730340aa683a98b37a8a0ca3df6ee18aad68d4d
Random06 <yashjosh7486@gmail.com>
2023-02-05 18:22:20 +0530

    se done

--- app.py
+++ app.py
@@ -1690,5 +1690,5 @@ def server_error():
 
 
 if __name__ == '__main__':
-    # app.run(host="0.0.0.0", port=3000)
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=3000)
+    # app.run(debug=True)
