ec2a351aed2568ff18976107b93286ce8b84955d
Random06 <yashjosh7486@gmail.com>
2023-02-05 11:10:26 +0530

    committing

--- app.py
+++ app.py
@@ -1524,5 +1524,5 @@ def server_error():
 
 
 if __name__ == '__main__':
-    # app.run(host="0.0.0.0", port=3000)
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=3000)
+    # app.run(debug=True)
